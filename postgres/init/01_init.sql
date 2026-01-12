8103ddf889fe063c58b07d41578c0a9c21f73b86
zituryoku0 <tommykouki1031@icloud.com>
2026-01-12 22:09:56 +0900

    <add>:userinfoテーブルにdelete_flgを追加

--- postgres/init/01_init.sql
+++ postgres/init/01_init.sql
@@ -9,7 +9,12 @@ CREATE TABLE userInfo (
 	userId char(32) NOT NULL,
 	userName char(64) NOT NULL,
 	userPassword char(64) NOT NULL,
-	latest_access_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+	latest_access_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	delete_flg char(1) NOT NULL DEFAULT '0'
 );
 -- サンプルデータの登録
 INSERT INTO userInfo (userId, userName, userPassword) VALUES('sampleUserId1', 'sample UserName1', 'abcdefgh');
+INSERT INTO userInfo (userId, userName, userPassword) VALUES('sampleUserId2', 'sample UserName2', 'ijklmnop');
+
+-- サンプルデータのアカウント無効化用
+update userInfo set latest_access_time = '2000-01-01 00:00:00 ' where userId = 'sampleUserId2';
